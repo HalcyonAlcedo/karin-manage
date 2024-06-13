4c1ec42217f1f3b0460087b2f4e97473b6175ce3
zyc404 <zyc404@gmail.com>
2024-06-13 19:37:05 +0800

    添加首页小组件

--- src/views/dashboards/default/DefaultDashboard.vue
+++ src/views/dashboards/default/DefaultDashboard.vue
@@ -10,6 +10,49 @@ import PluginCount from './components/PluginCount.vue';
 import RendererCount from './components/RendererCount.vue';
 import ManageUserCount from './components/ManageUserCount.vue';
 import MessageCount from './components/MessageCount.vue';
+
+import { loadModule } from "vue3-sfc-loader/dist/vue3-sfc-loader"
+import * as Vue from 'vue'
+import { onMounted, defineAsyncComponent, ref } from "vue"
+import { request } from '@/utils/request';
+import { useServerStore } from '@/stores/server';
+
+const apiStore = useServerStore();
+
+const widgets = ref([])
+
+onMounted(() => {
+  load()
+})
+
+// 加载远程文件
+const load = async () => {
+  let res = await request.post('/config/GetWidgets')
+  if (res.data?.data) {
+    for (const widget of res.data.data) {
+      const options = {
+        moduleCache: {
+          vue: Vue
+        },
+        async getFile() {
+          return widget.widget
+        },
+        addStyle(textContent) {
+          const style = Object.assign(document.createElement('style'), { textContent })
+          const ref = document.head.getElementsByTagName('style')[0] || null
+          document.head.insertBefore(style, ref)
+        },
+      }
+      // 加载远程组件
+      widgets.value.push({
+          plugin: widget.plugin,
+          file: widget.file,
+          data: widget.data,
+          remote: defineAsyncComponent(() => loadModule(widget.file, options))
+        })
+    }
+  }
+}
 </script>
 
 <template>
@@ -56,5 +99,10 @@ import MessageCount from './components/MessageCount.vue';
     <v-col cols="12" lg="5">
       <BotList />
     </v-col>
+    <!-- 远程组件 -->
+    <v-col v-for="widget in widgets" :key="widget" :lg="widget.col || 4" cols="12">
+      <component v-if="widget.remote" :is="widget.remote" :request="request" :apiUrl="apiStore.baseUrl" :data="widget.data" />
+    </v-col>
+
   </v-row>
 </template>
